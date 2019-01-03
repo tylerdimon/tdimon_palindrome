0ded0cf7eef8515a53445a92be9354e413b6332f
tylerdimon <tyler@nextupdate.com>
2019-01-02 19:50:03 -0600

    Initialize repository

---
+++ lib/tdimon_palindrome.rb
@@ -0,0 +1,6 @@
+require "tdimon_palindrome/version"
+
+module TdimonPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tdimon_palindrome/version.rb
@@ -0,0 +1,3 @@
+module TdimonPalindrome
+  VERSION = "0.1.0"
+end
